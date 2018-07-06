2a85aa5d82abdefe56056cf37e0742c1fc602012
hjx <827291356@qq.com>
2018-07-06 20:18:36 +0800

    挖矿

--- mining.js
+++ mining.js
@@ -1,30 +1,77 @@
 let apiMethod = require('./src/utils/apiMethod');
 
-(async function mining() {
+try {
+    (async function mining() {
 
-    let currentPrice
-    let myCurrencyAvailable = 0
-    let myCurrencyFrozen = 0
-    let currency = 'BDB'
+        let currentPrice
+        let myCurrencyAvailable = 0
+        let myCurrencyFrozen = 0
+        let currency = 'ICC'
+        let symbol = 'ICC_ETH'
+        let lastSalePrice = 0, lastSaleAmount = 0
+        let lastBuyPrice = 0, lastBuyAmount = 0
 
-    await apiMethod.getAccounts().then(res => {
-        typeof res === 'string' && (res = JSON.parse(res))
-        res.accounts && res.accounts.forEach((v, i) => {
-            if (v.currency == currency) {
-                if (v.type === 'SPOT_AVAILABLE') {
-                    myCurrencyAvailable += v.balance
+        let getAccount = () => apiMethod.getAccounts().then(res => {
+            typeof res === 'string' && (res = JSON.parse(res))
+            res.accounts && res.accounts.forEach((v, i) => {
+                if (v.currency == currency) {
+                    if (v.type === 'SPOT_AVAILABLE') {
+                        myCurrencyAvailable += v.balance
+                    }
+                    if (v.type === 'SPOT_FROZEN') {
+                        myCurrencyFrozen += v.balance
+                    }
                 }
-                if (v.type === 'SPOT_FROZEN') {
-                    myCurrencyFrozen += v.balance
-                }
-            }
+            })
         })
-    })
-
-    console.warn(`我的账户${currency}信息：${myCurrencyAvailable}可用,${myCurrencyFrozen}冻结`)
 
 
-    console.warn('end')
+        let getDepth = () => apiMethod.getSpecifiedTradeDepth('ICC_ETH',).then(res => {
+            typeof res === 'string' && (res = JSON.parse(res))
 
-})()
+            let lastBuy = res.buyOrders.sort((a, b) => b.price - a.price).shift()
+            let lastSale = res.sellOrders.sort((a, b) => a.price - b.price).shift()
+            lastBuyPrice = lastBuy.price
+            lastBuyAmount = lastBuy.amount
+            lastSalePrice = lastSale.price
+            lastSaleAmount = lastSale.amount
+        })
 
+
+        await Promise.all([getAccount(), getDepth()]).then(() => {
+            console.log(`我的账户${currency}信息：${myCurrencyAvailable}可用,${myCurrencyFrozen}冻结`)
+            console.log(`最新买价：${lastBuyPrice}，最新卖价：${lastSalePrice}，差价为${lastSalePrice - lastBuyPrice}`)
+
+            let toSalePrice = (lastSalePrice - ((lastSalePrice - lastBuyPrice) / 2)).toFixed(8)
+
+
+            if (toSalePrice < lastBuyPrice) return
+            if (toSalePrice > lastSalePrice) return
+
+            console.warn(`自动化订单价格为`, toSalePrice)
+
+            apiMethod.createOrder({
+                amount: 700,
+                customFeatures: 65536,
+                orderType: 'SELL_LIMIT',
+                price: toSalePrice,
+                symbol: symbol,
+            }, true).then(res => {
+                console.warn('下单成功！', res)
+            }).catch(err => {
+                console.warn('下单报错！', err)
+            })
+
+
+        }).catch(err => {
+            console.warn('promise all wrong', err)
+        })
+
+
+        console.warn('end')
+
+    })()
+
+} catch (e) {
+    console.warn('some thing go wrong', e)
+}
